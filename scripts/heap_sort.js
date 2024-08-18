ff164dd1f34252c808ab18679110dc78aaca2ec7
Pithani Yaswika <120516365+yaswika0311@users.noreply.github.com>
2024-08-18 17:23:56 +0530

    Update heap_sort.js

--- scripts/heap_sort.js
+++ scripts/heap_sort.js
@@ -29,8 +29,8 @@ function swap(i,j)
     div_update(divs[i],div_sizes[i],"red");//Height update
     div_update(divs[j],div_sizes[j],"red");//Height update
 
-    div_update(divs[i],div_sizes[i],"blue");//Color update
-    div_update(divs[j],div_sizes[j],"blue");//Color update
+    div_update(divs[i],div_sizes[i],"grey");//Color update
+    div_update(divs[j],div_sizes[j],"grey");//Color update
 }
 
 function max_heapify(n,i)
@@ -43,7 +43,7 @@ function max_heapify(n,i)
     {
         if(largest!=i)
         {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
+            div_update(divs[largest],div_sizes[largest],"grey");//Color update
         }
 
         largest=l;
@@ -55,7 +55,7 @@ function max_heapify(n,i)
     {
         if(largest!=i)
         {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
+            div_update(divs[largest],div_sizes[largest],"grey");//Color update
         }
 
         largest=r;
@@ -86,7 +86,7 @@ function heap_sort()
 
         max_heapify(i,0);
 
-        div_update(divs[i],div_sizes[i],"blue");//Color update
+        div_update(divs[i],div_sizes[i],"grey");//Color update
         div_update(divs[i],div_sizes[i],"green");//Color update
     }
     div_update(divs[i],div_sizes[i],"green");//Color update
